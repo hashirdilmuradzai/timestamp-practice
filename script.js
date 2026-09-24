78a90cbedbf70cb364ec842bf8b259c4386c589a
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-09-24 11:50:58 +0500

    Timestamp Practice

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-// let date = new Date();
-let now = new Date(2026, 8, 23);
+let now = new Date();
 
-console.log(now);
+now = now.getUTCHours();
 
-// now = now.getUTCHours();
+let previous = new Date(2026, 8, 23);
 
-console.log(now);
+console.log(previous);
 
-console.log("Date + Time + Timezone:", now.toString()); // Date + time + timezone sab deta hai.
-console.log("Sirf Date:", now.toDateString()); // Sirf date deta hai. Time aur timezone hata deta hai.
-console.log("Sirf Time + Timezone:", now.toTimeString()); // Sirf time + timezone deta hai.
-console.log("Standard UTC date/time format:", now.toISOString()); // Date ko ISO 8601 format mein deta hai.
+now = previous.getUTCHours();
+
+console.log(previous);
+
+console.log("Date + Time + Timezone:", previous.toString()); // Date + time + timezone sab deta hai.
+console.log("Sirf Date:", previous.toDateString()); // Sirf date deta hai. Time aur timezone hata deta hai.
+console.log("Sirf Time + Timezone:", previous.toTimeString()); // Sirf time + timezone deta hai.
+console.log("Standard UTC date/time format:", previous.toISOString()); // Date ko ISO 8601 format mein deta hai.
 
 // Birthday Calculation
 
 let date = new Date();
-let birthday = new Date("2002-1-24");
+
+console.log(date);
+
+let birthday = new Date("2003-8-4");
+
+console.log(birthday);
+
+let diff = date.getTime() - birthday.getTime();
+
+console.log(diff);
+
+let age = diff / (1000 * 60 * 60 * 24 * 365);
+
+console.log("Age: ", age);
